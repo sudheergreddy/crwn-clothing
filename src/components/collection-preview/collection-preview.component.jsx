9a8b0689081f69dc097868b389a35e6cfdda88bb
Sudheer <dheerans@gmail.com>
2020-05-25 22:39:05 +0300

    update our app with a shoppage with Collection preview component and collection item component.

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+function CollectionPreview(props){
+    return(
+        <div className="collection-preview">
+            <h1 className="title">{props.title.toUpperCase()}</h1>
+            <div className="preview">
+                {
+                    props.items.filter((item, idx) => idx < 4).map(item => {
+                        return <CollectionItem id={item.id} name={item.name} imageUrl={item.imageUrl} price={item.price}/>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CollectionPreview;
